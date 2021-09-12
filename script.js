22356b1d3903b108f5f7723be71c697e6ded535c
Misha <mihaeldyachenko@gmail.com>
2021-09-12 17:17:18 +0300

    lesson 2

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Вариант 1
+console.log('Учим JS')
+
+// Вариант 2
+console.log('JS')
+console.log('Учим')
+
+// Вариант 3
+console.log('Учим')  console.log('JS')
+
+// Вариант 4
+console.log('Учим');
+console.log('JS');
+
+// вариант 3 имеет ошибку так как в одной строке два console.log без точки запятой.
