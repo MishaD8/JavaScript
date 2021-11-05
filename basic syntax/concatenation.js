fc39b7b1572294e8b8f30227594b67b3aeb17834
Misha <mihaeldyachenko@gmail.com>
2021-11-06 00:05:00 +0200

    add basic syntax + solid 4

---
+++ basic syntax/concatenation.js
@@ -0,0 +1,13 @@
+let firstName = '  Joffrey';
+let greeting = 'Hello';
+let sign1 = ',';
+let sign2 = '!';
+let greet = greeting + sign1 + firstName + sign2;
+console.log(greet);
+
+let info = "We couldn't verify you mother's maiden name";
+let intro = 'Here is important information about your account security.';
+let str = '\n' ; 
+let concate = intro + str + info;
+console.log(concate);
+
