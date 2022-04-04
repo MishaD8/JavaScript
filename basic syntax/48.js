c0f22c70fb6ad3affaf107875385c3154943d9f9
Misha <mihaeldyachenko@gmail.com>
2022-04-04 23:37:24 +0300

    javascript folder move

--- basic syntax/48.js
+++ basic syntax/48.js
@@ -1,7 +1,7 @@
 // 48
-// свойства и методы как выражения
+// свойства и методы как выражения (проверка)
 
 
 const text = 'Never forget what you are, for surely the world will not';
 
-console.log(`First: ${text[0]}\nLast: ${text[text.length-1]}`);
+console.log(`First:${text[0]}\nLast:${text[text.length-1]}`);
